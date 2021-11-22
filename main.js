54356d7ba966e9acb7397f6c66816b75bbf52350
dya-create <78065221+dya-create@users.noreply.github.com>
2021-11-21 18:34:37 -0600

    .

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+var first=[];
 var selectedRow = null
 var URLArray = [];
 var contactArray = [];
@@ -14,10 +16,8 @@ function viewCurrentContact() {
     document.getElementById("emailID").value = currentContact.email;   
     document.getElementById("cityID").value = currentContact.city;   
     document.getElementById("stateID").value = currentContact.state;
-    
     document.getElementById("zipID").value = currentContact.zip;  
-
-    // Todo: Add additional fields.
+    
     document.getElementById("statusID").innerHTML = "Status: Viewing contact " + (currentContactIndex+1) + " of " + contactArray.length;
 }
 
@@ -28,28 +28,18 @@ function previous() {
     }
     currentContact = contactArray[currentContactIndex];
     viewCurrentContact();
-
-    //if(currentContactIndex==0){disableBtn()}
-
-    //function disableBtn() {
-        //document.getElementById("pre").disabled = true;
-     // }
-      
     
     // Todo: Disable previous button when currentContactIndex equal to 0.
     // Todo: Save changed items to contacts array and resort array.
 }
 
-function next() {
+function next(){
     if (currentContactIndex < (contactArray.length-1)) {
         currentContactIndex++;
     }
     currentContact = contactArray[currentContactIndex];
     viewCurrentContact();
-    console.log(currentContactIndex)
-    
-    // Todo: Disable next button when there is no next item.
-    // Todo: Save changed items to contacts array and resort array.
+
 }
 
 function onFormSubmit() {
@@ -70,6 +60,148 @@ function readFormData() {
     formData["cityID"] = document.getElementById("cityID").value;
     formData["stateID"] = document.getElementById("stateID").value;
     return formData;
+}
+
+function add() {
+    var zip = document.getElementById("zipID").value
+    console.log("zip:"+zip);
+
+    console.log("function getPlace(zip) { ... }");
+    var xhr = new XMLHttpRequest();
+
+    // Register the embedded handler function
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var result = xhr.responseText;
+            console.log("result:"+result);
+            var place = result.split(', ');
+            if ((document.getElementById("cityID").value == "") || (document.getElementById("cityID").value == " "))
+                document.getElementById("cityID").value = place[0];
+            if (document.getElementById("stateID").value == "")
+                document.getElementById("stateID").value = place[1];
+        }
+    }
+    xhr.open("GET", "getCityState.php?zip=" + zip);
+    xhr.send(null);
+}
+// Taken from W3 tutorial and modified
+function autocomplete(inp, arr) {
+    /*the autocomplete function takes two arguments,
+    the text field element and an array of possible autocompleted values:*/
+    var currentFocus;
+    /*execute a function when someone writes in the text field:*/
+    inp.addEventListener("input", function(e) {
+        var a, b, i, val = this.value;
+        /*close any already open lists of autocompleted values*/
+        closeAllLists();
+        if (!val) { return false;}
+        currentFocus = -1;
+        /*create a DIV element that will contain the items (values):*/
+        a = document.createElement("DIV");
+        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+        /*append the DIV element as a child of the autocomplete container:*/
+        this.parentNode.appendChild(a);
+        /*for each item in the array...*/
+        for (i = 0; i < arr.length; i++) {
+          /*check if the item starts with the same letters as the text field value:*/
+          if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+            /*create a DIV element for each matching element:*/
+            b = document.createElement("DIV");
+            /*make the matching letters bold:*/
+            b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+            b.innerHTML += arr[i].substr(val.length);
+            /*insert a input field that will hold the current array item's value:*/
+            b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+            /*execute a function when someone clicks on the item value (DIV element):*/
+            b.addEventListener("click", function(e) {
+                /*insert the value for the autocomplete text field:*/
+                inp.value = this.getElementsByTagName("input")[0].value;
+                /*close the list of autocompleted values,
+                (or any other open lists of autocompleted values:*/
+                closeAllLists();
+            });
+            a.appendChild(b);
+          }
+        }
+    });
+    /*execute a function presses a key on the keyboard:*/
+    inp.addEventListener("keydown", function(e) {
+        var x = document.getElementById(this.id + "autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
+        if (e.keyCode == 40) {
+          /*If the arrow DOWN key is pressed,
+          increase the currentFocus variable:*/
+          currentFocus++;
+          /*and and make the current item more visible:*/
+          addActive(x);
+        } else if (e.keyCode == 38) { //up
+          /*If the arrow UP key is pressed,
+          decrease the currentFocus variable:*/
+          currentFocus--;
+          /*and and make the current item more visible:*/
+          addActive(x);
+        } else if (e.keyCode == 13) {
+          /*If the ENTER key is pressed, prevent the form from being submitted,*/
+          e.preventDefault();
+          if (currentFocus > -1) {
+            /*and simulate a click on the "active" item:*/
+            if (x) x[currentFocus].click();
+          }
+        }
+    });
+    function addActive(x) {
+      /*a function to classify an item as "active":*/
+      if (!x) return false;
+      /*start by removing the "active" class on all items:*/
+      removeActive(x);
+      if (currentFocus >= x.length) currentFocus = 0;
+      if (currentFocus < 0) currentFocus = (x.length - 1);
+      /*add class "autocomplete-active":*/
+      x[currentFocus].classList.add("autocomplete-active");
+    }
+    function removeActive(x) {
+      /*a function to remove the "active" class from all autocomplete items:*/
+      for (var i = 0; i < x.length; i++) {
+        x[i].classList.remove("autocomplete-active");
+      }
+    }
+    function closeAllLists(elmnt) {
+      /*close all autocomplete lists in the document,
+      except the one passed as an argument:*/
+      var x = document.getElementsByClassName("autocomplete-items");
+      for (var i = 0; i < x.length; i++) {
+        if (elmnt != x[i] && elmnt != inp) {
+          x[i].parentNode.removeChild(x[i]);
+        }
+      }
+    }
+    /*execute a function when someone clicks in the document:*/
+    document.addEventListener("click", function (e) {
+        closeAllLists(e.target);
+    });
+  }
+
+// In conjunction with the above autocomplete function, showLookup() calls viewCurrentContact
+function showLookup(){
+
+    var contactLookedUp = document.getElementById("myInput").value;
+    var k = 0;
+    for(var i = 0; i<contactArray.length;i++){
+      
+        if(contactArray[i].firstName == contactLookedUp){
+            var k = 1;
+            document.getElementById("fullName").value = contactArray[i].firstName
+            document.getElementById("emailID").value = contactArray[i].email;   
+            document.getElementById("cityID").value = contactArray[i].city;   
+            document.getElementById("stateID").value = contactArray[i].state;
+            document.getElementById("zipID").value = contactArray[i].zip;  
+        }
+    }
+    if(k==0){
+        console.log()
+    }
+
 }
 
 function insertNewRecord(data) {
@@ -111,7 +243,7 @@ function updateRecord(formData) {
 }
 
 function onDelete(td) {
-    if (confirm('Are you sure to delete this record ?')) {
+    if (confirm('Do you want to delete contact?')) {
         row = td.parentElement.parentElement;
         document.getElementById("updates").deleteRow(row.rowIndex);
         resetForm();
@@ -130,7 +262,7 @@ function validate() {
     return isValid;
 }
 
-function add(){
+function newContact(){
     console.log('add()');
     var newContact = {
 
@@ -141,13 +273,15 @@ function add(){
         zip: document.getElementById("zipID").value ,
         
     }
+    first.push(newContact.firstName)
     contactArray.push(newContact);
     currentContactIndex = currentContactIndex + 1;
     viewCurrentContact();
+    document.getElementById("contactsID").innerHTML = JSON.stringify(contactArray,null,2);
 }
 
 function remove(){
-    if(contactArray,length >1){
+    if(contactArray.length >1){
         console.log('remove()');
         contactArray.splice(currentContactIndex,1)
         if(currentContactIndex>=1){
@@ -156,19 +290,42 @@ function remove(){
         console.log(contactArray)
         viewCurrentContact();
     }else{
-        console.log("Ennter one contact please")
+        console.log("Enter one contact please")
     }
 }
+// function stores contact objects into a contactArray and logs it to the DOM
+async function nextContact(URL) {
+   
+    const response = await fetch(URL);
+    const contactRequest = await response.text();
 
+    console.log(contactRequest);
 
-function zipFocusFunction() {
-    console.log('focusFunction()');
+    var contact;
+    contact = JSON.parse(contactRequest);
 
-    // Todo: Remove the function as it is not needed.
-}
+    var i = (contact.firstName);
+    
+    first.push(i);
+    autocomplete(document.getElementById("myInput"), first);
+   
+    contactArray.push(contact);
 
-function zipBlurFunction() {
-    getPlace();
+    document.getElementById("contactsID").innerHTML = JSON.stringify(contactArray,null,2);
+
+    document.getElementById("statusID").innerHTML = "Status: Loading " + contact.firstName + " " + contact.lastName;
+    loadingContact++;
+    if (contactURLArray.length > loadingContact) {
+        nextContact(contactURLArray[loadingContact]);
+    }
+    else {
+        document.getElementById("statusID").innerHTML = "Status: Contacts Loaded (" + contactURLArray.length + ")";
+        viewCurrentContact()
+        
+
+        //Todo: Sort contacts array.
+    }
+    
 }
 
 function keyPressed() {
@@ -180,7 +337,7 @@ function keyPressed() {
 async function loadIndex() {
     
     //fetch url
-    const response = await fetch("https://hostingjson.azurewebsites.net/index.json")
+    const response = await fetch("https://mustang-index.azurewebsites.net/index.json")
 
     //fetch text version
     const contactIndex = await response.text() 
@@ -190,7 +347,7 @@ async function loadIndex() {
     
 
     // fetching json version 
-    const responseTwo = await fetch("https://hostingjson.azurewebsites.net/index.json")
+    const responseTwo = await fetch("https://mustang-index.azurewebsites.net/index.json")
     const contactIndexTwo= await responseTwo.json() 
 
     for (i=0; i<contactIndexTwo.length; i++) {
@@ -245,7 +402,17 @@ async function loadNextContact(URL) {
 
 }
 
+
+
+
+
 // log contacts to console
 function logContacts() {
     console.log(contactArray);
+}
+
+
+function initApplication() {
+    //loadIndex();
+    console.log("hello, Mustang version 2 starting")
 }
